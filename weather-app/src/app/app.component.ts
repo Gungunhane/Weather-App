d864fead83d81de9c2c0bbd462ecb41f2f99b9c7
Sipson Wegner Ngale <sipsonngale@gmail.com>
2023-01-05 14:26:26 +0200

    Created the app and added fontawesome dependecies

---
+++ weather-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { faWind } from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'weather-app';
+  faWind = faWind;
+}
